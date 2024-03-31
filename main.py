0388d2f95446eef7fd0df4f96abb215cc6725f79
kirillkir2828 <151865160+kirillkir2828@users.noreply.github.com>
2024-03-31 21:04:56 +0300

    latest version of the telegram bot

--- main.py
+++ main.py
@@ -1,36 +1,90 @@
 import logging
-
-import telegram
+import time
 from telegram import ReplyKeyboardMarkup
 from telegram import ForceReply, Update
+from telegram import ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import requests
 
-reply_keyboard = [['/start', '/help']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+URL = 't.me/physics_training_bot'
+TOKEN = '6915498321:AAEG09W9StyUxgOqV7-wNw73RocE7I46msQ'
 
 
 async def start(update, context):
-    user = update.effective_user
+    reply_keyboard = [['/start', '/help'],
+                      ['/data', '/time']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text(
-        rf"Привет {user.mention_html()}! Я бот-тренажер по физике",
+        "Привет! 👋 \nЯ бот-тренажер по физике, давай вместе порешаем задачи?",
         reply_markup=markup
     )
-
-
-async def help(update, context):
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
 
 
 async def close_keyboard(update, context):
     await update.message.reply_text(
         "Ok",
-        reply_markup=ReplyKeyboardMarkup()
+        reply_markup=ReplyKeyboardRemove()
     )
 
 
-def main():
-    application = Application.builder().token("6195146605:AAE86fNa3FdK-pgTMwHKpyr1ZAXWZgWARzU").build()
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Как я могу помочь?")
+
+
+async def topic(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "Для того, чтобы выбрать раздел напиши его номер:\n1.Кинематика \n2.Динамика\n3.Статика")
+    """
+    if a == '1':
+        await update.message.reply_text(
+            f'Для того, чтобы выбрать подраздел напиши его номер:\nAзбука кинематики\nПрямолинейное равномерное движение')
+    """
+
+
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(time.asctime().split()[3])
+
+
+async def data_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(' '.join(time.asctime().split()[1:3]))
+
+
+async def echo(update, context):
+    #global a
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    a = update.message.reply_text(f'{update.message.text}')
+    await a
+    #await update.message.reply_text(f'{update.message.text}')
+
+
+def main() -> None:
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("6915498321:AAEG09W9StyUxgOqV7-wNw73RocE7I46msQ").build()
     application.add_handler(CommandHandler("close", close_keyboard))
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("topic", topic))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("time", time_command))
+    application.add_handler(CommandHandler("data", data_command))
+
+    # Run the bot until the user presses Ctrl-C
+    text_handler = MessageHandler(filters.TEXT, echo)
+    application.add_handler(text_handler)
     application.run_polling()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
